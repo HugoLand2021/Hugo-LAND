32ddef340429d7f6605e57c6dd721e5bb7a82c96
Antoine Dextraze <1765983@etu.cegepsth.qc.ca>
2021-02-10 16:33:20 -0500

    gj,sdbvkjsdghjkrdgkjsrhrfkdjgerjwlgherbsfhdjlgsrjlgsdhjlgjsrg

---
+++ Hugo LAND.Core/Model/MondeCRUD.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hugo_LAND.Core.Model
+{
+    public class MondeCRUD
+    {
+        //Méthode pour les Mondes
+        public static void CreerMonde(string description,int limX, int limY, )
+        {
+
+        }
+
+
+    }
+}
